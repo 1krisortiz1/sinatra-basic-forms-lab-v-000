94a6e139c600ce866fbca462fa27437f5522ae0d
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-07 15:05:40 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,9 +2,9 @@ class Puppy
     attr_accessor :name, :breed, :age
 
     def initialize(name, breed, age) #creates a new instance of the puppy class
-        @name = name
-        @breed = breed
-        @age = age
+        @name = name #can read a puppy name
+        @breed = breed #can read a puppy breed
+        @age = age #can read a puppy age (puppy#age)
     end
 
 end
